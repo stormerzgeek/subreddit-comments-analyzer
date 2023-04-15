2363be40bf8efc1c078d08b351176d6ffe5d23b5
stormerzgeek <78222170+stormerzgeek@users.noreply.github.com>
2023-04-15 09:12:31 +0530

    Typos

--- Test4.py
+++ Test4.py
@@ -12,26 +12,18 @@ TEMPLATE_DIR = os.path.abspath('./templates')
 STATIC_DIR = os.path.abspath('./static')
 
 app = Flask(__name__, template_folder=TEMPLATE_DIR, static_folder=STATIC_DIR)
-#app = Flask(__name__)
-
 redditApi = praw.Reddit(client_id='ehGX8eI976F8BsGzFy1PiQ', client_secret='ESUoHqETagk3jbp3NXBwy2OsgS9WsQ', user_agent='subreddit analysis by u/stormerzgeek')
 
 def recursive_node_adder(g, comment, parent_author):
     '''Recursively process comments and add them to the graph'''
-    
-    # Check if we have the node already in our graph
     if comment.author!=None:
         if comment.author not in g.nodes:
             g.add_node(comment.author)
-        # Create an edge between this comment author and the parent author
         g.add_edge(comment.author, parent_author)
-        # Iterate through the comments
         for reply in comment.replies.list():
             if isinstance(reply, praw.models.MoreComments):
                 continue
-            # Recursively process this reply
             recursive_node_adder(g, reply, comment.author)
-# Create an undirected graph
 def generate_graph(subreddit_name,postsno,sorting):
     g = nx.Graph()
     subreddit = subreddit_name
@@ -44,29 +36,22 @@ def generate_graph(subreddit_name,postsno,sorting):
     elif sorting=="top":
         submissions = targetSub.top(limit=postsno)
     for post in submissions:
-        #print (post.author, "-", post.title)
-        # Check if we have the node already in our graph
         if post.author!=None:
             if post.author not in g.nodes:
                 g.add_node(post.author)
             post.comment_limit = breadthCommentCount
-            # Get the top few comments
             for comment in post.comments.list():
-                # Skip MoreComment objects, which don't have authors
                 if isinstance(comment, praw.models.MoreComments):
                     continue
-                # Recursively process this reply
                 recursive_node_adder(g, comment, post.author)
     global gendata
     gendata = g
 
 def plotgraph(g,type,label):
-    #fig, ax = plt.subplots()
     fig = plt.figure(figsize=(10, 8))
     ax = fig.subplots()
     pos = nx.spring_layout(g, scale=200, iterations=5, k=0.2)
     pos_fr = nx.fruchterman_reingold_layout(g)
-    # And draw the graph with node labels
     print(label)
     if(type == "spring" and label == "true"):
         nx.draw(g,pos,ax=ax, node_color='red', width=1, with_labels=True, node_size=200)
@@ -100,7 +85,6 @@ def plotgraph(g,type,label):
             nx.draw_networkx(g, pos = pos, cmap = plt.get_cmap("jet"), node_color = values, node_size = 200, with_labels = False)
         elif(label=="true"):
             nx.draw_networkx(g, pos = pos, cmap = plt.get_cmap("jet"), node_color = values, node_size = 200, with_labels = True)
-    #plt.show()
     html_graph = mpld3.fig_to_html(fig)
     edges=g.number_of_edges()
     nodes=g.number_of_nodes()
@@ -108,7 +92,6 @@ def plotgraph(g,type,label):
 
 def dcentral(g):
     value = nx.degree_centrality(g)
-    #values=[(x,centrality[x]) for x in sorted(centrality,key=centrality.get,reverse=False)]
     return value
 
 def bcentral(g):
@@ -126,7 +109,7 @@ def density(g):
 
 def transitivity(g):
     value = nx.transitivity(g)
-    value_dict={"Global Coefficinet (Transitivity)":value}
+    value_dict={"Global Coefficient (Transitivity)":value}
     return value_dict
 
 def degree(g):
@@ -191,7 +174,6 @@ def measures():
         head_tag="Reddit Username"
         sortstat="(Click to sort)"
         values=degree(gendata)
-    #html_graph=plotgraph(finalgraph)
     return render_template('measures.html', type_name=type_name, values=values,head_tag=head_tag,sortstat=sortstat)
 
 @app.route('/download', methods=["GET","POST"])
